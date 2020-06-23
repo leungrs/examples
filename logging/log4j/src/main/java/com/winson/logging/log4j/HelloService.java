1883c70ac83f4c254b67929299781a3f417f1c63
Winson Liang <lai_mosi@126.com>
2020-06-23 21:21:46 +0800

    update

--- logging/log4j/src/main/java/com/winson/logging/log4j/HelloService.java
+++ logging/log4j/src/main/java/com/winson/logging/log4j/HelloService.java
@@ -6,8 +6,19 @@ import org.apache.logging.log4j.Logger;
 public class HelloService {
     private static Logger logger = LogManager.getLogger(HelloService.class);
 
+    private int a;
+
+    private String name;
+
+    private boolean flag;
+
     public String sayHello(String name) {
         Util.log(logger);
         return "Hello " + name;
+    }
+
+    @Override
+    public String toString() {
+        return name + "_" + flag + "_" + a;
     }
 }
